80c2bcef5dfc0a6730874c609822ef96961fbce0
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2026-04-09 17:07:24 -0400

    creation models cellier

--- caveo/app/Models/Cellier.php
+++ caveo/app/Models/Cellier.php
@@ -8,4 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Cellier extends Model
 {
     use HasFactory;
+
+    /**
+     * Nom de la table associée au modèle.
+     *
+     * @var string
+     */
+    protected $table = 'celliers';
+
+    /**
+     * Clé primaire du modèle.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributs pouvant être assignés en masse.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nom',
+        'id_utilisateur',
+        'description',
+        'emplacement',
+    ];
+
+    /**
+     * Retourne l'utilisateur propriétaire du cellier.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function utilisateur()
+    {
+        return $this->belongsTo(Utilisateur::class, 'id_utilisateur');
+    }
+
+    /**
+     * Retourne les lignes d'inventaire associées au cellier.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function inventaires()
+    {
+        return $this->hasMany(Inventaire::class, 'id_cellier');
+    }
 }
