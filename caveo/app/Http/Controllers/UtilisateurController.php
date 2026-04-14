de81f147b7efe947c392b25cb1cb6054ea3088d9
Greszta <00.Patrik.Greszta@gmail.com>
2026-04-14 16:37:34 -0400

    Ajout de la fonction show

--- caveo/app/Http/Controllers/UtilisateurController.php
+++ caveo/app/Http/Controllers/UtilisateurController.php
@@ -3,40 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Utilisateur;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+
 
 class UtilisateurController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
     /**
      * Display the specified resource.
      */
     public function show(Utilisateur $utilisateur)
     {
-        //
+        return view('profil.show', [
+            'user' => Auth::user()
+        ]);
     }
 
     /**
