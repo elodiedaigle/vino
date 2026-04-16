1a2631af30d47c1cb22b697c934a3bd7c799cba5
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2026-04-16 00:03:06 -0400

    réédentation

--- caveo/resources/views/bouteilles/show.blade.php
+++ caveo/resources/views/bouteilles/show.blade.php
@@ -1,175 +1,176 @@
 @extends('layouts.main')
 @section('title', 'Fiche détaillée')
 @section('fleche')
-    <!-- Flèche de retour qui revient à la page précédente (Cellier ou Catalogue) -->
-    <a href="{{ url()->previous() }}">
-        <img src="{{ asset('images/fleches/gauche-blanc.svg') }}" alt="Flèche de retour" class="w-10 h-10">
-    </a>
+<!-- Flèche de retour qui revient à la page précédente (Cellier ou Catalogue) -->
+<a href="{{ url()->previous() }}">
+    <img src="{{ asset('images/fleches/gauche-blanc.svg') }}" alt="Flèche de retour" class="w-10 h-10">
+</a>
 @endsection
 @section('content')
-    <script type="module" src="{{ asset('js/message-flash-auto.js') }}"></script>
-    <script type="module" src="{{ asset('js/confirmation-suppression-bouteille.js') }}"></script>
-    <!-- Titre de la page en fonction de l'origine (Cellier ou Catalogue) -->
-    <div class="px-3 pt-4 pb-28">
-        <h2 class="mb-3 text-center text-xl text-[#7A1E2E]" style="font-family: 'Roboto', sans-serif;">
-            {{ $source === 'cellier'
+<script type="module" src="{{ asset('js/message-flash-auto.js') }}"></script>
+<script type="module" src="{{ asset('js/confirmation-suppression.js') }}"></script>
+
+<!-- Titre de la page en fonction de l'origine (Cellier ou Catalogue) -->
+<div class="px-3 pt-4 pb-28">
+    <h2 class="mb-3 text-center text-xl text-[#7A1E2E]" style="font-family: 'Roboto', sans-serif;">
+        {{ $source === 'cellier'
         ? "Fiche détaillée de ma bouteille"
         : "Fiche détaillée de la bouteille" }}
-        </h2>
+    </h2>
 
-        <x-alerts />
-        <!-- Section Détails -->
-        <section>
-            <!-- Nom de la bouteille -->
-            <h3 class="mb-3 text-center text-l font-medium text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
-                {{ $bouteille->nom }}
-            </h3>
-            <!-- Section Image -->
-            <div class="relative mb-3 flex h-44 items-center justify-center">
-                <!-- Afficher l'image ou l'image par défaut si aucune image trouvée -->
-                @if ($bouteille->image)
-                    <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}" class="max-h-full max-w-full object-contain">
-                @else
-                    <img src="{{ asset('images/bouteilles/bouteille-placeholder.png') }}" alt="Image par défaut"
-                        class="max-h-full max-w-full object-contain">
-                @endif
-                <!-- Afficher la pastille de goût s'il y en a une -->
-                @if ($bouteille->image_pastille)
-                    <img src="{{ asset('images/pastilles/' . $bouteille->image_pastille) }}"
-                        alt="{{ $bouteille->pastille_gout }}" class="absolute right-8 bottom-4 h-16 w-16 object-contain">
-
-                @endif
-            </div>
-            <!-- Détails principaux-->
-            @if ($bouteille->type)
-                <p class="mb-1 text-left text-base font-normal text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
-                    {{ $bouteille->type }}
-                </p>
+    <x-alerts />
+    <!-- Section Détails -->
+    <section>
+        <!-- Nom de la bouteille -->
+        <h3 class="mb-3 text-center text-l font-medium text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
+            {{ $bouteille->nom }}
+        </h3>
+        <!-- Section Image -->
+        <div class="relative mb-3 flex h-44 items-center justify-center">
+            <!-- Afficher l'image ou l'image par défaut si aucune image trouvée -->
+            @if ($bouteille->image)
+            <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}" class="max-h-full max-w-full object-contain">
+            @else
+            <img src="{{ asset('images/bouteilles/bouteille-placeholder.png') }}" alt="Image par défaut"
+                class="max-h-full max-w-full object-contain">
             @endif
-            @if ($bouteille->pays)
-                <p class="mb-1 text-left text-base font-normal text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
-                    {{ $bouteille->pays }}
-                </p>
+            <!-- Afficher la pastille de goût s'il y en a une -->
+            @if ($bouteille->image_pastille)
+            <img src="{{ asset('images/pastilles/' . $bouteille->image_pastille) }}"
+                alt="{{ $bouteille->pastille_gout }}" class="absolute right-8 bottom-4 h-16 w-16 object-contain">
+
             @endif
-            <!-- Prix -->
-            <p class="mb-5 text-left text-base font-medium text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
-                @if ($bouteille->prix !== null)
-                    {{ number_format($bouteille->prix, 2, ',', ' ') }}$
-                @endif
-            </p>
-            <!-- Détails secondaires -->
-            <div class="mt-3 pt-3 border-t border-[#E0E0E0] text-left text-[#1A1A1A]"
-                style="font-family: 'Roboto', sans-serif;">
-                <!-- Millésime -->
-                @if ($bouteille->millesime)
-                    <div class="flex gap-2 mb-1 text-base">
-                        <span class="font-medium">Millésime :</span>
-                        <span class="flex-1 font-normal">
-                            {{ $bouteille->millesime}}
-                        </span>
-                    </div>
-                @endif
-
-                <!-- Taux d'alcool -->
-                @if ($bouteille->taux_alcool)
-                    <div class="flex gap-2 mb-1 text-base">
-                        <span class="font-medium">Taux d'alcool :</span>
-                        <span class="flex-1 font-normal">
-                            {{ $bouteille->taux_alcool ? $bouteille->taux_alcool . '%' : 'Non spécifié' }}
-                        </span>
-                    </div>
-                @endif
-
-                @if ($bouteille->cepage || $bouteille->format)
-                    <div class="flex justify-between items-start mb-1 text-base">
-                        <!-- Cépage(s) à gauche -->
-                        @if ($bouteille->cepage)
-                            <div class="flex gap-2 text-base">
-                                <span class="font-medium">Cépage(s) :</span>
-                                <span class="flex-1 font-normal">
-                                    {{ $bouteille->cepage }}
-                                </span>
-                            </div>
-                        @endif
-
-                        <!-- Format à droite -->
-                        @if ($bouteille->format)
-                            <span class="text-sm font-normal">
-                                {{ $bouteille->format }} ml
-                            </span>
-                        @endif
-                    </div>
-                @endif
-
-                <!-- Description à afficher seulement s'il y en a une -->
-                @if (!empty($bouteille->description))
-                    <div class="flex gap-2 mb-1 text-base">
-                        <span class="font-medium">Description :</span>
-                        <span class="flex-1 font-normal wrap-break-word">{{ $bouteille->description }}</span>
-                    </div>
-                @endif
+        </div>
+        <!-- Détails principaux-->
+        @if ($bouteille->type)
+        <p class="mb-1 text-left text-base font-normal text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
+            {{ $bouteille->type }}
+        </p>
+        @endif
+        @if ($bouteille->pays)
+        <p class="mb-1 text-left text-base font-normal text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
+            {{ $bouteille->pays }}
+        </p>
+        @endif
+        <!-- Prix -->
+        <p class="mb-5 text-left text-base font-medium text-[#1A1A1A]" style="font-family: 'Roboto', sans-serif;">
+            @if ($bouteille->prix !== null)
+            {{ number_format($bouteille->prix, 2, ',', ' ') }}$
+            @endif
+        </p>
+        <!-- Détails secondaires -->
+        <div class="mt-3 pt-3 border-t border-[#E0E0E0] text-left text-[#1A1A1A]"
+            style="font-family: 'Roboto', sans-serif;">
+            <!-- Millésime -->
+            @if ($bouteille->millesime)
+            <div class="flex gap-2 mb-1 text-base">
+                <span class="font-medium">Millésime :</span>
+                <span class="flex-1 font-normal">
+                    {{ $bouteille->millesime}}
+                </span>
             </div>
-            <!-- Gestion et affichage des quantités (Cellier + Inventaire)  -->
-            @if ($source === 'cellier' && $inventaire)
-                <div class="mt-4 border-t border-[#E0E0E0] pt-4">
-                    <div class="mx-auto flex w-fit items-center justify-center gap-10">
-                        @if($inventaire->quantite == 0)
-                            <form method="POST" action="{{ route('inventaires.destroy', $inventaire) }}" class="inline-flex">
-                                @csrf
-                                @method('DELETE')
+            @endif
 
-                                <button type="submit"
-                                    class="bouton-supprimer px-2 py-2 border border-gray-300 text-gray-600 rounded hover:bg-gray-100 flex items-center justify-center"
-                                    data-confirm="Supprimer cette bouteille ?" aria-label="Supprimer la bouteille">
-                                    <img src="{{ asset('images/icons/poubelle.svg') }}" alt="" aria-hidden="true" class="w-6 h-6">
-                                </button>
-                            </form>
-                        @else
-                            <form method="POST" action="{{ route('inventaires.updateQuantite', $inventaire) }}" class="inline-flex">
-                                @csrf
-                                @method('PATCH')
+            <!-- Taux d'alcool -->
+            @if ($bouteille->taux_alcool)
+            <div class="flex gap-2 mb-1 text-base">
+                <span class="font-medium">Taux d'alcool :</span>
+                <span class="flex-1 font-normal">
+                    {{ $bouteille->taux_alcool ? $bouteille->taux_alcool . '%' : 'Non spécifié' }}
+                </span>
+            </div>
+            @endif
 
-                                <input type="hidden" name="source_page" value="bouteille">
-                                <input type="hidden" name="id_bouteille" value="{{ $bouteille->id }}">
-                                <input type="hidden" name="quantite" value="{{ max(0, $inventaire->quantite - 1) }}">
-
-                                <button type="submit" class="flex items-center justify-center" aria-label="Diminuer la quantité"
-                                    title="Diminuer la quantité">
-                                    <img src="{{ asset('images/icons/cercle-moins.svg') }}" alt="moins" aria-hidden="true"
-                                        class="w-10 h-10">
-                                </button>
-                            </form>
-                        @endif
-
-                        <div class="min-w-[32px] text-center">
-                            <span class="text-2xl font-semibold text-[#1A1A1A]">
-                                {{ $inventaire->quantite }}
-                            </span>
-                        </div>
-
-                        <form method="POST" action="{{ route('inventaires.updateQuantite', $inventaire) }}" class="inline-flex">
-                            @csrf
-                            @method('PATCH')
-
-                            <input type="hidden" name="source_page" value="bouteille">
-                            <input type="hidden" name="id_bouteille" value="{{ $bouteille->id }}">
-                            <input type="hidden" name="quantite" value="{{ min(999, $inventaire->quantite + 1) }}">
-
-                            <button type="submit" class="flex items-center justify-center" aria-label="Augmenter la quantité"
-                                title="Augmenter la quantité">
-                                <img src="{{ asset('images/icons/cercle-plus.svg') }}" alt="plus" aria-hidden="true"
-                                    class="w-10 h-10">
-                            </button>
-                        </form>
-                    </div>
-
-                    @if($inventaire->quantite == 0)
-                        <p class="mt-3 text-center text-xs text-red-500">
-                            Cette bouteille est conservée dans le cellier, mais elle a été bue.
-                        </p>
-                    @endif
+            @if ($bouteille->cepage || $bouteille->format)
+            <div class="flex justify-between items-start mb-1 text-base">
+                <!-- Cépage(s) à gauche -->
+                @if ($bouteille->cepage)
+                <div class="flex gap-2 text-base">
+                    <span class="font-medium">Cépage(s) :</span>
+                    <span class="flex-1 font-normal">
+                        {{ $bouteille->cepage }}
+                    </span>
                 </div>
+                @endif
+
+                <!-- Format à droite -->
+                @if ($bouteille->format)
+                <span class="text-sm font-normal">
+                    {{ $bouteille->format }} ml
+                </span>
+                @endif
+            </div>
             @endif
-        </section>
-    </div>
+
+            <!-- Description à afficher seulement s'il y en a une -->
+            @if (!empty($bouteille->description))
+            <div class="flex gap-2 mb-1 text-base">
+                <span class="font-medium">Description :</span>
+                <span class="flex-1 font-normal wrap-break-word">{{ $bouteille->description }}</span>
+            </div>
+            @endif
+        </div>
+        <!-- Gestion et affichage des quantités (Cellier + Inventaire)  -->
+        @if ($source === 'cellier' && $inventaire)
+        <div class="mt-4 border-t border-[#E0E0E0] pt-4">
+            <div class="mx-auto flex w-fit items-center justify-center gap-10">
+                @if($inventaire->quantite == 0)
+                <form method="POST" action="{{ route('inventaires.destroy', $inventaire) }}" class="inline-flex">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit"
+                        class="bouton-supprimer px-2 py-2 border border-gray-300 text-gray-600 rounded hover:bg-gray-100 flex items-center justify-center"
+                        data-confirm="Supprimer cette bouteille ?" aria-label="Supprimer la bouteille">
+                        <img src="{{ asset('images/icons/poubelle.svg') }}" alt="" aria-hidden="true" class="w-6 h-6">
+                    </button>
+                </form>
+                @else
+                <form method="POST" action="{{ route('inventaires.updateQuantite', $inventaire) }}" class="inline-flex">
+                    @csrf
+                    @method('PATCH')
+
+                    <input type="hidden" name="source_page" value="bouteille">
+                    <input type="hidden" name="id_bouteille" value="{{ $bouteille->id }}">
+                    <input type="hidden" name="quantite" value="{{ max(0, $inventaire->quantite - 1) }}">
+
+                    <button type="submit" class="flex items-center justify-center" aria-label="Diminuer la quantité"
+                        title="Diminuer la quantité">
+                        <img src="{{ asset('images/icons/cercle-moins.svg') }}" alt="moins" aria-hidden="true"
+                            class="w-10 h-10">
+                    </button>
+                </form>
+                @endif
+
+                <div class="min-w-[32px] text-center">
+                    <span class="text-2xl font-semibold text-[#1A1A1A]">
+                        {{ $inventaire->quantite }}
+                    </span>
+                </div>
+
+                <form method="POST" action="{{ route('inventaires.updateQuantite', $inventaire) }}" class="inline-flex">
+                    @csrf
+                    @method('PATCH')
+
+                    <input type="hidden" name="source_page" value="bouteille">
+                    <input type="hidden" name="id_bouteille" value="{{ $bouteille->id }}">
+                    <input type="hidden" name="quantite" value="{{ min(999, $inventaire->quantite + 1) }}">
+
+                    <button type="submit" class="flex items-center justify-center" aria-label="Augmenter la quantité"
+                        title="Augmenter la quantité">
+                        <img src="{{ asset('images/icons/cercle-plus.svg') }}" alt="plus" aria-hidden="true"
+                            class="w-10 h-10">
+                    </button>
+                </form>
+            </div>
+
+            @if($inventaire->quantite == 0)
+            <p class="mt-3 text-center text-xs text-red-500">
+                Cette bouteille est conservée dans le cellier, mais elle a été bue.
+            </p>
+            @endif
+        </div>
+        @endif
+    </section>
+</div>
 @endsection
