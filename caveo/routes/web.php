38d02585757a0219c8092fe3ee723b921caa3eb4
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2026-04-20 15:35:15 -0400

    update  web.php pour retirer les use non utilisés

--- caveo/routes/web.php
+++ caveo/routes/web.php
@@ -9,8 +9,6 @@ use App\Http\Controllers\InventaireController;
 use App\Http\Controllers\ListeAchatController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UtilisateurController;
-use App\Models\Bouteille;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
